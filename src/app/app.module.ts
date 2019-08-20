dc0470081ed7de035843e17fe9df03df94e258f6
Mihaela Bondor <mihaela_bondor@yahoo.com>
2019-08-20 14:32:28 +0300

    routing deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,22 +5,12 @@ import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {ReadJsonComponent} from './read-json/read-json.component';
 import {HttpClientModule} from '@angular/common/http';
-import {LoginComponent} from './login/login.component';
-import {DashboardComponent} from './dashboard/dashboard.component';
-import {UsersComponent} from './users/users.component';
-import {BugsComponent} from './bugs/bugs.component';
-import {RolesComponent} from './roles/roles.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ReadJsonComponent,
     ReadJsonComponent,
-    LoginComponent,
-    DashboardComponent,
-    UsersComponent,
-    BugsComponent,
-    RolesComponent
   ],
   imports: [
     BrowserModule,
