26f388a6f1a7cb423504475f1d0880dabe605b1f
Mihaela Bondor <mihaela_bondor@yahoo.com>
2019-08-21 14:50:48 +0300

    post pentru rolePermission

--- src/app/roles/role.permission.ts
+++ src/app/roles/role.permission.ts
@@ -1,12 +1,28 @@
-import {OnInit} from "@angular/core";
+import {Role} from "../models/role.model";
+import {Permission} from "../models/permission.model";
 
-export class RolePermission implements OnInit {
+export class RolePermission {
+  private _roleDTO: Role;
+  private _permissionDTO: Permission;
 
-
-  constructor() {
+  constructor(roleDTO: Role, permissionDTO: Permission) {
+    this._roleDTO = roleDTO;
+    this._permissionDTO = permissionDTO;
   }
 
-  ngOnInit() {
+  get roleDTO(): Role {
+    return this._roleDTO;
   }
 
+  set roleDTO(value: Role) {
+    this._roleDTO = value;
+  }
+
+  get permissionDTO(): Permission {
+    return this._permissionDTO;
+  }
+
+  set permissionDTO(value: Permission) {
+    this._permissionDTO = value;
+  }
 }
--- src/app/roles/roles.service.ts
+++ src/app/roles/roles.service.ts
@@ -2,13 +2,13 @@ import {Role} from "../models/role.model";
 import {Permission} from "../models/permission.model";
 import {BackendService} from "../core/backend/backend.service";
 import {Observable} from "rxjs";
-import * as Http from "http";
+import {RolePermission} from "./role.permission";
 
 export class RolesService {
   private _roles: Role[];
   private _permissions: Permission[];
 
-  constructor(private backendService: BackendService, private http: Http) {
+  constructor(private backendService: BackendService) {
   }
 
   getAllRoles(): Observable<Role[]> {
@@ -19,5 +19,7 @@ export class RolesService {
     return this.backendService.get('http://localhost:8080/jbugs/api/permissions/');
   }
 
-
+  sendData(rolePermission: RolePermission) {
+    this.backendService.post('http://localhost:8080/jbugs/api/roles/', rolePermission).subscribe();
+  }
 }
