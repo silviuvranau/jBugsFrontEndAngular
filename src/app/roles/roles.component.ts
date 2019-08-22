dfcda6a9f637d54f65a5883fde540fe847bb0230
Mihaela Bondor <mihaela_bondor@yahoo.com>
2019-08-22 09:45:46 +0300

    cleaning

--- src/app/roles/roles.component.ts
+++ src/app/roles/roles.component.ts
@@ -1,8 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {Role} from '../models/role.model';
-import {Permission} from "../models/permission.model";
-import {RolesService} from "./roles.service";
-import {RolePermission} from "./role.permission";
+import {Permission} from '../models/permission.model';
+import {RolesService} from './roles.service';
+import {RolePermission} from './role.permission';
 
 @Component({
   selector: 'app-roles',
@@ -19,14 +19,14 @@ export class RolesComponent implements OnInit {
   }
 
   ngOnInit() {
+    // get list with roles from backend
     this.roleService.getAllRoles().subscribe(obj => {
       this.roles = obj;
-      console.log(this.roles.length);
     });
 
+    // get list with permissions from backend
     this.roleService.getAllPermissions().subscribe(obj => {
       this.permissions = obj;
-      console.log(this.permissions.length);
     });
   }
 
@@ -39,12 +39,14 @@ export class RolesComponent implements OnInit {
     return false;
   }
 
+  /**
+   * Method for sending new checkbox changes to backend
+   * @param per
+   * @param role
+   */
   onCheckboxClick(per: Permission, role: Role) {
-    const perJSON = JSON.parse(JSON.stringify(per));
-    const roleJSON = JSON.parse(JSON.stringify(role));
+    // create Wrapper with role and permission given from table
     this.rolePermission = new RolePermission(role, per);
-    const rolePermissionJson = JSON.parse(JSON.stringify(this.rolePermission));
-    console.log(rolePermissionJson);
 
     this.roleService.sendData(this.rolePermission).subscribe((data: {}) => {
       console.log(data);
--- src/app/roles/roles.service.ts
+++ src/app/roles/roles.service.ts
@@ -1,16 +1,12 @@
-import {Role} from "../models/role.model";
-import {Permission} from "../models/permission.model";
-import {BackendService} from "../core/backend/backend.service";
-import {Observable} from "rxjs";
-import {RolePermission} from "./role.permission";
-import {HttpClient} from "@angular/common/http";
+import {Role} from '../models/role.model';
+import {Permission} from '../models/permission.model';
+import {BackendService} from '../core/backend/backend.service';
+import {Observable} from 'rxjs';
+import {RolePermission} from './role.permission';
 
 export class RolesService {
-  private _roles: Role[];
-  private _permissions: Permission[];
-  public token: string;
 
-  constructor(private backendService: BackendService, private http: HttpClient) {
+  constructor(private backendService: BackendService) {
   }
 
   getAllRoles(): Observable<Role[]> {
@@ -22,15 +18,7 @@ export class RolesService {
   }
 
   sendData(rolePermission: RolePermission): Observable<any> {
-    /**
-     // console.log(rolePermission.permissionDTO.id, rolePermission.permissionDTO.type, rolePermission.roleDTO.id, rolePermission.roleDTO.type, rolePermission.roleDTO.permissions.length);
-    this.backendService.post('http://localhost:8080/jbugs/api/roles/', rolePermission).subscribe();
-     // console.log(rolePermission.permissionDTO.id, rolePermission.roleDTO.id);**/
     return this.backendService.post('http://localhost:8080/jbugs/api/roles/', rolePermission);
   }
 
-  private extractData(res: Response) {
-    const body = res;
-    return body || {};
-  }
 }
