5ae6ee89df5d7835c7f3afe3a886576f1bea28a3
Radu Ungureanu <Radu.UNGUREANU@student.utcluj.ro>
2019-08-28 17:09:16 +0300

    added notifications-list

---
+++ src/app/notification/notification.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { BackendService } from '../core/backend/backend.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NotificationService {
+
+  constructor(private backendService: BackendService) { }
+
+  public getAllNotificationsForUser(username : string): Observable<Notification[]>{
+    return this.backendService.get("http://localhost:8080/jbugs/api/notifications/" + username);
+  }
+}
